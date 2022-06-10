c8b287c3c631d74b459fc1d1a1e2fa3996ea1a00
arigatodl <99845398+dulguun-staderlabs@users.noreply.github.com>
2022-06-10 12:57:18 +0200

    wip

---
+++ bots/index.js
@@ -0,0 +1,19 @@
+const { BncClient, Stake } = require("@binance-chain/javascript-sdk");
+
+(async () => {
+  try {
+    const client = new BncClient("https://api.binance.org/bc/");
+    // const client = new BncClient("https://dex-atlantic.binance.org");
+    await client.initChain(56);
+    const balance = await client.getTxs(
+      "bnb1xgnms7dsnydz6zjr9na9rv2sz7aw3ydhacg7wc",
+      1653762060000,
+      1654539735365
+    );
+    console.log(balance);
+  } catch (e) {
+    console.log(e);
+    // Deal with the fact the chain failed
+  }
+  // `text` is not available here
+})();
