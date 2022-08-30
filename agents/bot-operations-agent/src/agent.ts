7fffc271838aa825de26d79e2e9f8887e506babc
Manoj Patra <manoj9april@gmail.com>
2022-08-31 00:10:27 +0530

    startDelegation, completeDelegation delay completed

--- agents/bot-operations-agent/src/agent.ts
+++ agents/bot-operations-agent/src/agent.ts
@@ -7,24 +7,36 @@ import {
   TransactionEvent,
 } from "forta-agent";
 import {
+  COMPLETE_DELEGATION_DELAY,
+  COMPLETE_DELEGATION_FN,
   protocol,
-  REWARD_CHANGE_PCT_THRESHOLD,
+  REWARD_CHANGE_BPS,
   REWARD_DELAY_HOURS,
   REWARD_EVENT,
   STAKE_MANAGER,
+  START_DELEGATION_DELAY,
+  START_DELEGATION_FN,
+  TOTAL_BPS,
 } from "./constants";
 
 import { getHours } from "./utils";
 
-let dailyRewardsAlerted: boolean,
+let dailyRewardsFailed: boolean,
   lastRewardsTime: Date,
   lastRewardAmount: BigNumber;
+
+let lastStartDelegationTime: Date, startDelegationFailed: boolean;
+let lastCompleteDelegationTime: Date, completeDelegationFailed: boolean;
 
 const handleTransaction: HandleTransaction = async (
   txEvent: TransactionEvent
 ) => {
   const findings: Finding[] = (
-    await Promise.all([handleRewardTransaction(txEvent)])
+    await Promise.all([
+      handleRewardTransaction(txEvent),
+      handleStartDelegationTransaction(txEvent),
+      handleCompleteDelegationTransaction(txEvent),
+    ])
   ).flat();
 
   return findings;
@@ -43,12 +55,12 @@ const handleRewardTransaction: HandleTransaction = async (
         lastRewardAmount
           .sub(_amount)
           .abs()
-          .gt(lastRewardAmount.mul(REWARD_CHANGE_PCT_THRESHOLD).div(100))
+          .gt(lastRewardAmount.mul(REWARD_CHANGE_BPS).div(TOTAL_BPS))
       ) {
         findings.push(
           Finding.fromObject({
             name: "Significant Reward Change",
-            description: `Reward changed more than ${REWARD_CHANGE_PCT_THRESHOLD} %`,
+            description: `Reward changed more than ${REWARD_CHANGE_BPS} %`,
             alertId: "BNBx-5",
             protocol: protocol,
             severity: FindingSeverity.High,
@@ -63,24 +75,24 @@ const handleRewardTransaction: HandleTransaction = async (
     }
 
     lastRewardsTime = new Date();
-    dailyRewardsAlerted = false;
+    dailyRewardsFailed = false;
     lastRewardAmount = _amount;
 
     return findings;
   }
 
-  if (!lastRewardsTime) {
-    return findings;
-  }
+  if (!lastRewardsTime) return findings;
+
+  if (dailyRewardsFailed) return findings;
 
   const currentTime = new Date();
   const diff = currentTime.getTime() - lastRewardsTime.getTime();
   const diffHours = getHours(diff);
-  if (diffHours > REWARD_DELAY_HOURS && !dailyRewardsAlerted) {
+  if (diffHours > REWARD_DELAY_HOURS) {
     findings.push(
       Finding.fromObject({
-        name: "Rewards Delay",
-        description: `Rewards not Autocompunded`,
+        name: "Daily Rewards Failed",
+        description: `Daily Rewards Autocompund not invoked since ${REWARD_DELAY_HOURS} Hours`,
         alertId: "BNBx-6",
         protocol: protocol,
         severity: FindingSeverity.High,
@@ -90,7 +102,98 @@ const handleRewardTransaction: HandleTransaction = async (
         },
       })
     );
-    dailyRewardsAlerted = true;
+    dailyRewardsFailed = true;
+  }
+
+  return findings;
+};
+
+const handleStartDelegationTransaction: HandleTransaction = async (
+  txEvent: TransactionEvent
+) => {
+  const findings: Finding[] = [];
+  const startDelegationInvocations = txEvent.filterFunction(
+    START_DELEGATION_FN,
+    STAKE_MANAGER
+  );
+
+  if (startDelegationInvocations.length) {
+    lastStartDelegationTime = new Date();
+    startDelegationFailed = false;
+    return findings;
+  }
+
+  if (!lastStartDelegationTime) return findings;
+
+  if (startDelegationFailed) return findings;
+
+  const currentTime = new Date();
+  const diff = currentTime.getTime() - lastStartDelegationTime.getTime();
+  const diffHours = getHours(diff);
+  if (diffHours > START_DELEGATION_DELAY) {
+    findings.push(
+      Finding.fromObject({
+        name: "Start Delegation Failed",
+        description: `Start Delegation not invoked since ${START_DELEGATION_DELAY} Hours`,
+        alertId: "BNBx-7",
+        protocol: protocol,
+        severity: FindingSeverity.High,
+        type: FindingType.Info,
+        metadata: {
+          lastStartDelegationTime: lastStartDelegationTime.toUTCString(),
+        },
+      })
+    );
+    startDelegationFailed = true;
+  }
+
+  return findings;
+};
+
+const handleCompleteDelegationTransaction: HandleTransaction = async (
+  txEvent: TransactionEvent
+) => {
+  const findings: Finding[] = [];
+  const completeDelegationInvocations = txEvent.filterFunction(
+    COMPLETE_DELEGATION_FN,
+    STAKE_MANAGER
+  );
+
+  if (completeDelegationInvocations.length) {
+    lastCompleteDelegationTime = new Date();
+    completeDelegationFailed = false;
+    return findings;
+  }
+
+  if (!lastStartDelegationTime || !lastCompleteDelegationTime) {
+    return findings;
+  }
+
+  if (startDelegationFailed || completeDelegationFailed) return findings;
+
+  const currentTime = new Date();
+  const diff = currentTime.getTime() - lastStartDelegationTime.getTime();
+  const compDiff = currentTime.getTime() - lastCompleteDelegationTime.getTime();
+
+  const diffHours = getHours(diff);
+  const compDiffHours = getHours(compDiff);
+
+  if (diffHours > COMPLETE_DELEGATION_DELAY && compDiffHours > diffHours) {
+    findings.push(
+      Finding.fromObject({
+        name: "Complete Delegation Failed",
+        description: `Complete Delegation not invoked since ${COMPLETE_DELEGATION_DELAY} Hours of last Start Delegation`,
+        alertId: "BNBx-8",
+        protocol: protocol,
+        severity: FindingSeverity.High,
+        type: FindingType.Info,
+        metadata: {
+          lastStartDelegationTime: lastStartDelegationTime.toUTCString(),
+          lastCompleteDelegationTime: lastCompleteDelegationTime.toUTCString(),
+        },
+      })
+    );
+    completeDelegationFailed = true;
   }
 
   return findings;
