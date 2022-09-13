021924b2247b272f68f2489dfd2613681b0071f4
Manoj Patra <manoj9april@gmail.com>
2022-09-13 19:57:47 +0530

    updated run.sh

--- certora/run.sh
+++ certora/run.sh
@@ -1,14 +1,3 @@
-if [[ "$1" ]]
-then
-    RULE="--rule $1"
-fi
-
-if [[ "$2" ]]
-then
-    MSG=": $2"
-fi
-
-
 certoraRun  certora/harness/StakeManagerHarness.sol \
             contracts/StakeManager.sol \
             contracts/BnbX.sol \
@@ -19,5 +8,4 @@ certoraRun  certora/harness/StakeManagerHarness.sol \
 --loop_iter 3 \
 --settings -optimisticFallback=true --optimistic_loop \
 --staging \
-$RULE  \
---msg "bnbx -$RULE $MSG"
+--msg "bnbx"
