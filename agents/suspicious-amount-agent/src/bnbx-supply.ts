0c3f1f1a4da08b6679116f0c3e3f9cf5231cf6ee
Manoj Patra <manoj9april@gmail.com>
2022-12-13 12:52:22 +0530

    BNB-29 | Forta Integration (#38)
    * forta init
    
    * wip
    
    * added high bnbx mint, high bnbx unstake
    
    * wip
    
    * rewards pct change done
    
    * startDelegation, completeDelegation delay completed
    
    * bot undelegation failures tracked
    
    * readme update
    
    * er-drop, bnbx-supply done
    
    * README pdate
    
    * minor changes
    
    * small changes
    
    * bots published
    
    * added reorg fix
    
    * fix: added blockNumber impl.
    
    * updated logs
    
    Co-authored-by: Dulguun <99845398+dulguun-staderlabs@users.noreply.github.com>

---
+++ agents/suspicious-amount-agent/src/bnbx-supply.ts
@@ -0,0 +1,119 @@
+import {
+  BlockEvent,
+  ethers,
+  EventType,
+  Finding,
+  FindingSeverity,
+  FindingType,
+  getEthersProvider,
+  HandleBlock,
+} from "forta-agent";
+import {
+  BNBx,
+  BNBX_SUPPLY_CHANGE_HOURS,
+  BNBX_SUPPLY_CHANGE_PCT,
+  protocol,
+  STAKE_MANAGER,
+} from "./constants";
+
+import abis from "./abi";
+import { BigNumber } from "ethers";
+import { getHours } from "./utils";
+
+let lastSupply: BigNumber, lastSupplyTime: Date;
+let supplyMismatch: boolean;
+
+const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
+  const findings: Finding[] = [];
+
+  if (blockEvent.type === EventType.REORG) return findings;
+
+  const bnbX = new ethers.Contract(BNBx, abis.BnbX.abi, getEthersProvider());
+  const stakeManager = new ethers.Contract(
+    STAKE_MANAGER,
+    abis.StakeManager.abi,
+    getEthersProvider()
+  );
+
+  const oneEther = ethers.utils.parseEther("1");
+  const currentER: BigNumber = await stakeManager.convertBnbXToBnb(oneEther, {
+    blockTag: blockEvent.blockNumber,
+  });
+  const totalPooledBnb: BigNumber = await stakeManager.getTotalPooledBnb({
+    blockTag: blockEvent.blockNumber,
+  });
+  const currentSupply: BigNumber = await bnbX.totalSupply({
+    blockTag: blockEvent.blockNumber,
+  });
+  const currentSupplyTime: Date = new Date();
+
+  if (
+    currentER
+      .mul(currentSupply)
+      .div(oneEther)
+      .sub(totalPooledBnb)
+      .abs()
+      .div(oneEther)
+      .gt(1)
+  ) {
+    if (!supplyMismatch) {
+      findings.push(
+        Finding.fromObject({
+          name: "BNBx Supply Mis-Match",
+          description: `BNBx, ER and TotalPooledBnb doesn't match`,
+          alertId: "BNBx-SUPPLY-MISMATCH",
+          protocol: protocol,
+          severity: FindingSeverity.Critical,
+          type: FindingType.Exploit,
+          metadata: {
+            currentER: currentER.toString(),
+            totalPooledBnb: totalPooledBnb.toString(),
+            currentSupply: currentSupply.toString(),
+          },
+        })
+      );
+      supplyMismatch = true;
+    }
+  } else {
+    supplyMismatch = false;
+  }
+
+  if (!lastSupply) {
+    lastSupply = currentSupply;
+    lastSupplyTime = currentSupplyTime;
+    return findings;
+  }
+  const diffHours = getHours(
+    currentSupplyTime.getTime() - lastSupplyTime.getTime()
+  );
+  if (diffHours > BNBX_SUPPLY_CHANGE_HOURS) {
+    if (
+      currentSupply
+        .sub(lastSupply)
+        .abs()
+        .gt(lastSupply.mul(BNBX_SUPPLY_CHANGE_PCT).div(100))
+    ) {
+      findings.push(
+        Finding.fromObject({
+          name: "BNBx Supply Change",
+          description: `BNBx Total Supply changed more than ${BNBX_SUPPLY_CHANGE_PCT} %`,
+          alertId: "BNBx-SUPPLY-CHANGE",
+          protocol: protocol,
+          severity: FindingSeverity.High,
+          type: FindingType.Suspicious,
+          metadata: {
+            lastSupply: lastSupply.toString(),
+            currentSupply: currentSupply.toString(),
+          },
+        })
+      );
+    }
+
+    lastSupplyTime = currentSupplyTime;
+    lastSupply = currentSupply;
+  }
+
+  return findings;
+};
+
+export { handleBlock };
---
+++ agents/suspicious-amount-agent/src/utils.ts
@@ -0,0 +1,5 @@
+const getHours = (miliSecs: number) => {
+  return miliSecs / (1000 * 60 * 60);
+};
+
+export { getHours };
